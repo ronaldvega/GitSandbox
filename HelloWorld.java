c89b7004b6e5be862cb60245e21cd7248ebf732f
ronaldvega <ronaldvega112375@gmail.com>
2014-05-03 13:20:46 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
     public static void main(String[] args){
 	    System.out.println("Hello World! I am Ronald. I'm using GIT!");
-		System.out.println("I love Git!");
+	    System.out.println("I love Git!");
+	    System.out.println("Editing this one on GitHub.");
 	}
 }
