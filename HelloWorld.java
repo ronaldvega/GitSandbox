aae75bf4e599cca5ec15f4971e71850b86644174
ronald vega <ronaldvega112375@gmail.com>
2014-05-03 13:23:58 +0800

    Added to the print output

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
 	    System.out.println("Hello World! I am Ronald. I'm using GIT!");
 	    System.out.println("I love Git!");
 	    System.out.println("Editing this one on GitHub.");
+		System.out.println("This one is edited from local.");
 	}
 }
